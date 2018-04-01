f335682e4b5b28f688c615e7e197a01b1ae90e47
Isuru Anuradha <Isuruanuradha99@gmail.com>
2018-04-01 15:42:39 +0800

    Task model modified

--- emiratetest/src/main/java/com/test/emiratetest/model/Task.java
+++ emiratetest/src/main/java/com/test/emiratetest/model/Task.java
@@ -1,8 +1,47 @@
 package com.test.emiratetest.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "task")
 public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "name")
+	private String status;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
 }
