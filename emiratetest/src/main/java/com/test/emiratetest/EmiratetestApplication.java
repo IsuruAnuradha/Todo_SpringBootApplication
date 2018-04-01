c4dec0950c3a49a7c9997ac814ec05b9cb421c99
Isuru Anuradha <Isuruanuradha99@gmail.com>
2018-04-01 10:08:44 +0800

    Project Emirate Test Created

---
+++ emiratetest/src/main/java/com/test/emiratetest/EmiratetestApplication.java
@@ -0,0 +1,12 @@
+package com.test.emiratetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiratetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmiratetestApplication.class, args);
+	}
+}
